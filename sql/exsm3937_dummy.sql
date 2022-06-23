7d8601d068d2980fd321888f34e27f786e42377f
Jean-Marc Delisle <delisle.jeanmarc@hotmail.com>
2022-06-23 10:46:56 -0600

    Fixed issue with James on syntax error, phase 1 transaction completed;

--- sql/exsm3937_dummy.sql
+++ sql/exsm3937_dummy.sql
@@ -19,29 +19,30 @@ CREATE TABLE Product (
     quantity_in_stock int NOT NULL
 );
 
+DROP TABLE IF EXISTS Order_Header;
 CREATE TABLE Order_Header( 
     order_header_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    total_price DECIMAL(15,2) NOT NULL,
-    FOREIGN KEY (order_header_id) REFERENCES customer(customer_id)  
+    total_price DECIMAL(15,2) NOT NULL
 );
+
+ALTER TABLE `order_header` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer`(`customer_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 DROP TABLE IF EXISTS order_header;
 CREATE TABLE Order_Detail(
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
-    order_header_id int NOT NULL FOREIGN KEY order_header_id REFERENCES order_header order_header_id,
-    product_id int NOT NULL FOREIGN KEY product_id REFERENCES product product_id,
+    order_header_id int NOT NULL,
+    product_id int NOT NULL,
     order_qty int NOT NULL,
     sub_total DECIMAL(15,2) NOT NULL
 );
 
+ALTER TABLE `order_detail` 
+ADD FOREIGN KEY (`order_header_id`) 
+REFERENCES `order_header`(`order_header_id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT; 
 
-ALTER TABLE
-    order_detail
-ADD
-    FOREIGN KEY order_header_id REFERENCES order_header order_header_id ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-ALTER TABLE
-    order_detail
-ADD
-    FOREIGN KEY product_id REFERENCES product product_id ON DELETE RESTRICT ON UPDATE RESTRICT;
-
+ALTER TABLE `order_detail` 
+ADD FOREIGN KEY (`product_id`) 
+REFERENCES `product`(`product_id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT;
---
+++ sql/transactions_01.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS jedelisl
+START TRANSACTION;
+
+SELECT * FROM jedelisl
+
+CREATE DATABASE jedelisl;
+
+START TRANSACTION;
+
+CREATE TABLE Customer (
+    customer_id int PRIMARY key AUTO_INCREMENT,
+    customer_name varchar(50) NOT NULL,
+    customer_email varchar(50) NULL,
+    street_address varchar(50) NULL,
+    city varchar(50) NULL,
+    province char(2) NULL,
+    postal_code char(6) NULL
+);
+
+CREATE TABLE Product (
+    product_id int PRIMARY key AUTO_INCREMENT,
+    product_name varchar(50) NOT NULL,
+    product_description varchar(200) NOT NULL,
+    price decimal(15, 2) NOT NULL,
+    quantity_in_stock int NOT NULL
+);
+
+CREATE TABLE Order_Header( 
+    order_header_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    total_price DECIMAL(15,2) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+CREATE TABLE Order_Detail(
+    order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_header_id int NOT NULL,
+    product_id int NOT NULL,
+    order_qty int NOT NULL,
+    sub_total DECIMAL(15,2) NOT NULL,
+    FOREIGN KEY (order_header_id) REFERENCES Order_Header(order_header_id),
+    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+);
+
+COMMIT;
+
+/* Below works when using xampp
+
+ALTER TABLE `order_header` 
+ADD FOREIGN KEY (`customer_id`) 
+REFERENCES `customer`(`customer_id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+ALTER TABLE `order_detail` 
+ADD FOREIGN KEY (`order_header_id`) 
+REFERENCES `order_header`(`order_header_id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT; 
+
+ALTER TABLE `order_detail` 
+ADD FOREIGN KEY (`product_id`) 
+REFERENCES `product`(`product_id`) 
+ON DELETE RESTRICT ON UPDATE RESTRICT;*/
+
+COMMIT;
