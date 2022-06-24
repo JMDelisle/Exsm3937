fdd9e49cd0df5ef99505d57ddacbaa6f8cfd12ec
Jean-Marc Delisle <delisle.jeanmarc@hotmail.com>
2022-06-23 18:08:21 -0600

    left off at query 4, trying to solve query 4;

--- sql/transactions_03.sql
+++ sql/transactions_03.sql
@@ -6,7 +6,7 @@ GROUP BY customer.customer_id
 ORDER BY COUNT(order_header_id) DESC, customer_name;
 
 /* Query 02 */
-SELECT customer_name AS Customer, SUM(total_price) AS `Sum of Purchase Totals`
+SELECT customer_name AS Customer, CONCAT('$', SUM(total_price)) AS `Sum of Purchase Totals`
 FROM customer 
 INNER JOIN order_header on customer.customer_id = order_header.customer_id
 GROUP BY customer.customer_id
@@ -14,19 +14,35 @@ ORDER BY SUM(total_price) DESC, customer_name;
 
 /* Query 03 */
 
-SELECT product_name AS Product, COUNT(order_qty) AS `Number of Products Sold`
-FROM product
-INNER JOIN order_qty ON product.product_name = order_detail.order_qty
+SELECT SUM(order_qty) AS `Number of Products Sold`, product_name
+FROM order_detail
+INNER JOIN product on order_detail.product_id = product.product_id
 GROUP BY product.product_name
-ORDER By COUNT(order_qty) DESC, product_name; 
-
-
-
-SELECT product.name, order_detail.order_qty, COUNT(order_qty)
-    FROM product
-    INNER JOIN productsubcategory on product.name=productsubcategory.ProductCategoryID
-    INNER JOIN product on productsubcategory.ProductSubcategoryID=product.ProductSubcategoryID
-WHERE product.DiscontinuedDate IS NULL
-GROUP BY product.name, order_detail.order_qty;
+ORDER BY SUM(order_qty) DESC, product_name ASC;
 
 /* Query 04 */
+SELECT order_header.order_header_id, product.product_name, order_detail.order_qty, customer.customer_name, order_header.order_date, order_header.total_price
+FROM order_header
+INNER JOIN product on order_header.product_name = product.product_name
+INNER JOIN order_detail on order_header.order_qty = order_detail.order_qty
+INNER JOIN customer on order_header.customer_id = customer.customer_name
+
+
+
+SELECT order_header.order_header_id, product.product_name, order_detail.order_qty, customer.customer_name, order_header.order_date, order_header.total_price
+From Order_Header
+    INNER JOIN
+
+
+
+
+
+SELECT order_header.order_header_id, product.product_name, order_detail.order_qty, customer.customer_name, order_header.order_date, order_header.total_price
+FROM order_header
+INNER JOIN product on order_header.product_name = product.product_name
+INNER JOIN order_detail on order_header.order_qty = order_detail.order_qty
+INNER JOIN customer on order_header.customer_id = customer.customer_name
+
+WHERE order_header.order_date =< 2022-01-07;
+
+/* Query 05 */
